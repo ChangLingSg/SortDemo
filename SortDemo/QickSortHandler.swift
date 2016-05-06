f466ee618b977eaf04e8029c302113ee85e2ac0d
ChangLing <changling@singtel.com>
2016-05-06 11:56:36 +0800

    add new func
    create quick sort handler

---
+++ SortDemo/QickSortHandler.swift
@@ -0,0 +1,17 @@
+//
+//  QickSortHandler.swift
+//  SortDemo
+//
+//  Created by Singtel on 6/5/16.
+//  Copyright © 2016 ChangLing. All rights reserved.
+//
+
+import UIKit
+
+class QickSortHandler: NSObject {
+
+    class func sort(inout array:[Int],left:Int,right:Int){
+    
+    }
+    
+}
